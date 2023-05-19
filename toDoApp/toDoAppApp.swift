92dfe16b9ec70d22ba5a245d0f5431f82af18a5b
fady <fady>
2023-05-19 16:05:07 +0200

    Initial Commit

---
+++ toDoApp/toDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoAppApp.swift
+//  toDoApp
+//
+//  Created by Folio on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct toDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
